a8169cc59c9c136adb9d28ab9d79ecd91b3f734d
Enrique Alvarez <enrique@codecantor.com>
2015-06-08 21:26:36 +0200

    Added scroller

--- src/assets/js/scrolls.js
+++ src/assets/js/scrolls.js
@@ -1,5 +1,41 @@
+const handlerSpy = function() {
+    $(window).off('scroll');
+    var menu = {};
+
+    $('article, footer').each(function() {
+        var $el = $(this);
+        menu[$el.offset().top] = $el.prop('id');
+    });
+
+    var tops = Object.keys(menu).sort(function(a, b){return b-a});
+    var plusTop = $(window).height() / 2;
+
+    scrollHandler = function() {
+        var top = $(this).scrollTop() + plusTop;
+
+        for(var i=0; i < tops.length; i++) {
+            if((top - tops[i]) >= 0 ) {
+                var $el = $("a[href='#" + menu[tops[i]] + "']");
+                if(!$el.hasClass('current')) {
+                    $('.current').removeClass('current');
+                    $el.addClass('current');
+                }
+                return true;
+            }
+        }
+
+    }
+
+    $(window).on('scroll', scrollHandler );
+    scrollHandler();
+}
+
 $(function() {
-    $('adfsa').on('click', function(){
-        return 'vvvasdf';
+    $(window).on('resize', handlerSpy);
+    handlerSpy();
+
+    $('nav a, .readmore a').on('click', function() {
+        $.scrollTo($($(this).attr('href')), 500);
+        return false;
     });
 });
